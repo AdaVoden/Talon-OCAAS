fe0030667703482a6647a8d208ec48e05df36743
Harlan Shaw <harlan.shaw@ucalgary.ca>
2021-12-03 12:10:09 -0700

    Actually compiling now, fixed error with scope.c building

--- src/libs/libmisc/catalogs.h
+++ src/libs/libmisc/catalogs.h
@@ -1,5 +1,7 @@
 /* prototypes for functions in catalogs.c */
 
+#include "circum.h"
+
 extern int searchCatalog (char catdir[], char catalog[], char source[],
     Obj *op, char message[]);
 extern int readCatalog (char fn[], Obj **opp, char message[]);
